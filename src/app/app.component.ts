73ea49c4ca9f8601e87fbb08cf1ba77d4d1f5654
tom.mansion <tomansion@yahoo.fr>
2025-05-02 17:21:44 +0200

    V1 Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,42 +28,124 @@ export class AppComponent {
 
   onRangeSubmit(range: VocalRange) {
     this.selectedRange = range;
+    console.log('Selected Range:', this.selectedRange);
   }
 
-  getHighlightRanges(): HighlightRange[] {
-    if (!this.selectedRange) {
-      return [];
+  getChestVoiceHighlightRanges(): HighlightRange[] {
+    if (!this.selectedRange) return [];
+
+    const ranges: HighlightRange[] = [];
+
+    // Add mid range
+    if (
+      this.selectedRange.register1MidStart &&
+      this.selectedRange.register1MidStop
+    ) {
+      ranges.push({
+        from: this.selectedRange.register1MidStart,
+        to: this.selectedRange.register1MidStop,
+        color: 'blue',
+      });
     }
 
-    const ranges: HighlightRange[] = [
-      {
-        from: this.selectedRange.register1ExtremeHigh,
+    // Add high range
+    if (
+      this.selectedRange.register1MidStop &&
+      this.selectedRange.register1High
+    ) {
+      ranges.push({
+        from: this.selectedRange.register1MidStop,
         to: this.selectedRange.register1High,
-        color: 'red',
-      },
-      {
-        from: this.selectedRange.register1Mid,
-        to: this.selectedRange.register1Low,
-        color: 'blue',
-      },
-      {
-        from: this.selectedRange.register1Fry,
-        to: this.selectedRange.register2ExtremeHigh,
-        color: 'green',
-      },
-      {
-        from: this.selectedRange.register2High,
-        to: this.selectedRange.register2Mid,
-        color: 'yellow',
-      },
-      {
-        from: this.selectedRange.register2Low,
-        to: 'C8',
         color: 'purple',
-      },
-    ];
+      });
+    }
 
-    console.log('ranges', ranges);
+    // Add Extreme high range
+    if (
+      this.selectedRange.register1High &&
+      this.selectedRange.register1ExtremeHigh
+    ) {
+      ranges.push({
+        from: this.selectedRange.register1High,
+        to: this.selectedRange.register1ExtremeHigh,
+        color: 'red',
+      });
+    }
+    // Add low range
+    if (
+      this.selectedRange.register1Low &&
+      this.selectedRange.register1MidStart
+    ) {
+      ranges.push({
+        from: this.selectedRange.register1Low,
+        to: this.selectedRange.register1MidStart,
+        color: 'purple',
+      });
+    }
+    // Add fry range
+    if (this.selectedRange.register1Fry && this.selectedRange.register1Low) {
+      ranges.push({
+        from: this.selectedRange.register1Fry,
+        to: this.selectedRange.register1Low,
+        color: 'red',
+      });
+    }
+
+    console.log(ranges);
+
+    return ranges;
+  }
+  getHeadVoiceHighlightRanges(): HighlightRange[] {
+    if (!this.selectedRange) return [];
+
+    const ranges: HighlightRange[] = [];
+
+    // Add mid range
+    if (
+      this.selectedRange.register2MidStart &&
+      this.selectedRange.register2MidStop
+    ) {
+      ranges.push({
+        from: this.selectedRange.register2MidStart,
+        to: this.selectedRange.register2MidStop,
+        color: 'blue',
+      });
+    }
+
+    // Add high range
+    if (
+      this.selectedRange.register2MidStop &&
+      this.selectedRange.register2High
+    ) {
+      ranges.push({
+        from: this.selectedRange.register2MidStop,
+        to: this.selectedRange.register2High,
+        color: 'purple',
+      });
+    }
+
+    // Add Extreme high range
+    if (
+      this.selectedRange.register2High &&
+      this.selectedRange.register2ExtremeHigh
+    ) {
+      ranges.push({
+        from: this.selectedRange.register2High,
+        to: this.selectedRange.register2ExtremeHigh,
+        color: 'red',
+      });
+    }
+    // Add low range
+    if (
+      this.selectedRange.register2Low &&
+      this.selectedRange.register2MidStart
+    ) {
+      ranges.push({
+        from: this.selectedRange.register2Low,
+        to: this.selectedRange.register2MidStart,
+        color: 'purple',
+      });
+    }
 
     return ranges;
   }
--- src/app/components/vocal-range-form/vocal-range-form.component.ts
+++ src/app/components/vocal-range-form/vocal-range-form.component.ts
@@ -12,15 +12,17 @@ import { MatButtonModule } from '@angular/material/button';
 import { debounceTime } from 'rxjs/operators';
 
 export interface VocalRange {
-  register1ExtremeHigh: string;
-  register1High: string;
-  register1Mid: string;
-  register1Low: string;
-  register1Fry: string;
-  register2ExtremeHigh: string;
-  register2High: string;
-  register2Mid: string;
-  register2Low: string;
+  register1ExtremeHigh?: string;
+  register1High?: string;
+  register1MidStop?: string;
+  register1MidStart?: string;
+  register1Low?: string;
+  register1Fry?: string;
+  register2ExtremeHigh?: string;
+  register2High?: string;
+  register2MidStart?: string;
+  register2MidStop?: string;
+  register2Low?: string;
 }
 
 @Component({
@@ -44,15 +46,18 @@ export class VocalRangeFormComponent {
 
   ngOnInit() {
     this.rangeForm = this.fb.group({
-      register1ExtremeHigh: ['D#4', Validators.required],
-      register1High: ['G3', Validators.required],
-      register1Mid: ['E2', Validators.required],
-      register1Low: ['C2', Validators.required],
-      register1Fry: ['B1', Validators.required],
-      register2ExtremeHigh: ['A4', Validators.required],
-      register2High: ['F4', Validators.required],
-      register2Mid: ['D4', Validators.required],
-      register2Low: ['B3', Validators.required],
+      register1ExtremeHigh: [''],
+      register1High: ['G3'],
+      register1MidStop: ['A#2'],
+      register1MidStart: ['B1'],
+      register1Low: ['G1'],
+      register1Fry: ['E1'],
+
+      register2ExtremeHigh: [''],
+      register2High: ['C#4'],
+      register2MidStop: ['G#3'],
+      register2MidStart: ['C3'],
+      register2Low: ['A#2'],
     });
 
     // Listen for form changes and emit the updated values
@@ -61,7 +66,18 @@ export class VocalRangeFormComponent {
         console.log('Form value changed:', value);
 
         this.rangeSubmit.emit(value as VocalRange);
+      } else {
+        console.log('Form is invalid');
+        // Display the full error messages
+        Object.keys(this.rangeForm.controls).forEach((key) => {
+          const control = this.rangeForm.get(key);
+          if (control && control.invalid) {
+            console.log(`${key} is invalid:`, control.errors);
+          }
+        });
       }
     });
+
+    this.rangeSubmit.emit(this.rangeForm.value as VocalRange);
   }
 }
